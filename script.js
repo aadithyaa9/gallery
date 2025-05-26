4e7ab79378b7fe45d3bdb732f0040a791b280643
fujitsu11 <aadiramki01@gmail.com>
2025-05-26 17:18:09 +0530

    Adding interactivity

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const filterButtons = document.querySelectorAll('.filter-btn');
+    const galleryItems = document.querySelectorAll('.gallery-item');
+    const fullscreenOverlay = document.getElementById('fullscreen-overlay');
+    const fullscreenImg = document.getElementById('fullscreen-img');
+    const fullscreenCaption = document.getElementById('fullscreen-caption');
+    const closeBtn = document.querySelector('.close-btn');
+    const themeSwitch = document.getElementById('theme-switch');
+    const body = document.body;
+
+})
