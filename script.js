e8435cf914e1754524fc175e562b158408330cb1
fujitsu11 <aadiramki01@gmail.com>
2025-05-26 17:23:22 +0530

    Adding toggling between themes

--- script.js
+++ script.js
@@ -7,5 +7,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeBtn = document.querySelector('.close-btn');
     const themeSwitch = document.getElementById('theme-switch');
     const body = document.body;
-
-})
+const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        body.classList.add(savedTheme);
+        if (savedTheme === 'dark-mode') {
+            themeSwitch.checked = true;
+        }
+    } else {body.classList.add('light-mode');
+    }
+     themeSwitch.addEventListener('change', () => {
+        if (themeSwitch.checked) {
+            body.classList.remove('light-mode');
+            body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark-mode');
+        } else {
+            body.classList.remove('dark-mode');
+            body.classList.add('light-mode');
+            localStorage.setItem('theme', 'light-mode');
+        }
+    });
+});
